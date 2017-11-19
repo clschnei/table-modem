9599b0014c7222eb645f328886991a57cde32ab0
Chris Schneider <clschnei@gmail.com>
2017-11-19 14:33:30 -0500

    migrate to JSDOM

--- server.js
+++ server.js
@@ -1,78 +1,80 @@
-const VALID_URL = /(http|https):\/\/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
+const { JSDOM } = require('jsdom');
+const express = require('express');
+const morgan = require('morgan');
+const request = require('request');
+
 const PORT = process.env.PORT || 5000;
-const express = require("express");
-const cheerio = require("cheerio");
-const request = require("request");
-const morgan = require("morgan");
-
+const VALID_URL = /(http|https):\/\/[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/gi;
 const app = express();
-let $ = null;
 
-const getData = function(selector = 'table') {
-  if ($(selector).is("table")) {
-    return getTableData(selector);
-  } else {
-    return (() => {
-      const result = [];
-      for (let element of Array.from($(selector))) {
-        const t = $(element).text();
-        if (t === "0") {
-          result.push(+t);
-        } else {
-          result.push(+t || t);
-        }
-      }
-      return result;
-    })();
+const getValue = (fullText) => {
+  const text = fullText.replace(/(\n|\t)/g, '').trim();
+
+  if (text === '0') {
+    return Number(text);
   }
+
+  return Number(text) || text;
 };
 
-const getTableData = function(selector) {
-  let t;
-  if (selector == null) {
-    selector = "table";
+const getTableData = (el) => {
+  const rows = Array.from(el.querySelectorAll('tr'));
+  return rows.map((row) => {
+    const cols = Array.from(row.querySelectorAll('th,td'));
+    return cols.map(col => getValue(col.textContent));
+  });
+};
+
+const getData = (elements) => {
+  const data = Array.from(elements).map((e) => {
+    if (e.tagName.match(/table/i)) {
+      return getTableData(e);
+    }
+
+    return getValue(e.textContent);
+  });
+
+  if (data.length === 1) {
+    return data[0];
   }
-  return Array.from($(selector)).map(table =>
-    Array.from($(table).find("tr")).map(tr =>
-      Array.from($(tr).find("td,th")).map(td => t = $(td).text().trim())
-    )
-  );
+
+  return data;
 };
 
 app.use(morgan());
 
-app.get("*", function(req, res) {
-  const { url, selector } = req.query;
+app.get('*', (req, res) => {
+  const { url, selector = 'table' } = req.query;
   // make sure url is kosher
   if (!(url && url.match(VALID_URL))) {
-    return res.send(400, { error: "must be a valid url" });
+    return res.send(400, { error: 'must be a valid url' });
   }
 
   // set response headers
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.setHeader("Content-Type", "application/json");
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'X-Requested-With',
+    'Content-Type': 'application/json',
+  });
 
   // fetch and construct table data
-  return request(url, function(e, r, b) {
+  return request(url, (e, r, body) => {
     if (e) {
       return res.send(404, e);
     }
-    $ = cheerio.load(b, { normalizeWhitespace: true });
-    const data = {
-      title: $("title").text().trim(),
-      data: getData(selector)
-    };
 
-    // if only one result, push it to the top-level
-    if (data.data.length === 1) {
-      data.data = data.data.pop();
-    }
+    const { window: { document } } = new JSDOM(body);
+
+    const data = {
+      title: document.querySelector('title').textContent.trim(),
+      data: getData(document.querySelectorAll(selector)),
+    };
 
     // return data
     if (!data.data.length) {
-      return res.send(404, { error: "url has no table data" });
+      return res.send(404, { error: `url has no "${selector}" data` });
     }
+
     return res.send(200, JSON.stringify(data));
   });
 });
